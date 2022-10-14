f7a598441dd66cb49412ec5bdd097d440925b465
JordyPirata <103045128+JordyPirata@users.noreply.github.com>
2022-10-13 20:20:02 -0500

    Se agregaron Multiples scripts
    Se agregó MapGeneratorEditor (Permite ejecura código fuera de PlayMode), MapGenerator (Clase Principal encargada de las funcionalidades), MapDisplay (Es la clase que se encarga de aplicar las texturas)

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,25 @@
+using Terrains;
+using UnityEditor;
+using UnityEngine;
+// versión 0e1010444a
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.AutoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
